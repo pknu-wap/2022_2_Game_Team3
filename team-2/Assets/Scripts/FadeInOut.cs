9c9b4cd59103f1bf1937fe68f082601fbed27d68
InSange <ehfrur888@naver.com>
2022-11-22 23:48:35 +0900

    Add GameOver in Maze & Jump

--- team-2/Assets/Scripts/FadeInOut.cs
+++ team-2/Assets/Scripts/FadeInOut.cs
@@ -6,11 +6,17 @@ using UnityEngine.UI;
 public class FadeInOut : MonoBehaviour
 {
     public Image fadeImage;    // UGUI의 Image 컴포넌트 참조 변수.
+    public Image GameOverImage;
     public Player player;
 
     public void FadeFunc()
     {
         StartCoroutine(FadeInStart());
+    }
+
+    public void GameOverFadeFunc()
+    {
+        StartCoroutine(GameOverFadeInStart());
     }
 
     IEnumerator FadeInStart()
@@ -31,6 +37,10 @@ public class FadeInOut : MonoBehaviour
 
     IEnumerator FadeOutStart()
     {
+        if(GameOverImage.gameObject.activeSelf == true)
+        {
+            GameOverImage.gameObject.SetActive(false);
+        }
         float fadetime = 1.0f;
         while(fadetime >= 0f)
         {
@@ -41,5 +51,21 @@ public class FadeInOut : MonoBehaviour
 
         fadeImage.gameObject.SetActive(false);
         player.isLoading = false;
+    }
+
+    IEnumerator GameOverFadeInStart()
+    {
+        GameOverImage.gameObject.SetActive(true);
+        float fadetime = 0f;
+        while (fadetime < 1.0f)
+        {
+            //Debug.Log(fadetime);
+            fadetime += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            GameOverImage.color = new Color(255, 0, 0, fadetime);
+        }
+
+        yield return new WaitForSeconds(2.0f);
+        StartCoroutine(FadeInStart());
     }
 }
