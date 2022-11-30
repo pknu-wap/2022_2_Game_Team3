92ebb917bdf7ca975d07155c912a7f6e1f9730dc
InSange <ehfrur888@naver.com>
2022-12-01 00:40:09 +0900

    RSP Room Setting V2

--- team-2/Assets/Scripts/RSP.cs
+++ team-2/Assets/Scripts/RSP.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class RSP : MonoBehaviour
 {
     enum RCPNum {
-        Paper = 4,
+        Rock = 4,
         Scissor = 5,
-        Rock = 6,
+        Paper = 6,
     };
     [SerializeField] string playerRCP;
     [SerializeField] string randomRCP;
 
+    public GameManager gameManager;
+    public SystemManager systemManger;
+    public Player player;
     public GameObject[] screen_s;
     bool isPlay;
 
@@ -38,18 +41,78 @@ public class RSP : MonoBehaviour
         int randomNum = Random.Range(4,7);
         randomRCP = ((RCPNum)randomNum).ToString();
         Debug.Log("randomRCP ÀÇ °ª : " + randomRCP);
-
+        
+        screen_s[7].SetActive(false);
+        screen_s[8].SetActive(false);
+        screen_s[0].SetActive(true);
+        // CountDown 3,2,1
         yield return new WaitForSeconds(2.0f);
         screen_s[0].SetActive(false);
         screen_s[1].SetActive(true);
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(1.0f);
         screen_s[1].SetActive(false);
         screen_s[2].SetActive(true);
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(1.0f);
         screen_s[2].SetActive(false);
         screen_s[3].SetActive(true);
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(1.0f);
         screen_s[3].SetActive(false);
         screen_s[randomNum].SetActive(true);
+        StartCoroutine(CompareGame(playerRCP, randomRCP));
+    }
+
+    IEnumerator CompareGame(string playerRSP, string randomRSP)
+    {
+        yield return new WaitForSeconds(2.0f);
+        if(playerRSP == "Rock")
+        {
+            if(randomRSP == "Scissor")
+            {
+                PlayerWinCount++;
+                screen_s[(int)RCPNum.Scissor].SetActive(false);
+                screen_s[7].SetActive(true);
+            }
+            else
+            {
+                screen_s[(int)RCPNum.Scissor].SetActive(false);
+                screen_s[8].SetActive(false);
+                player.live = false;
+                gameManager.GameOver();
+            }
+        }
+        else if(playerRSP == "Scissor")
+        {
+            if(randomRSP == "Paper")
+            {
+                PlayerWinCount++;
+                screen_s[(int)RCPNum.Paper].SetActive(false);
+                screen_s[7].SetActive(true);
+            }
+            else
+            {
+                screen_s[(int)RCPNum.Paper].SetActive(false);
+                screen_s[8].SetActive(false);
+                player.live = false;
+                gameManager.GameOver();
+            }
+        }
+        else if(playerRSP == "Paper")
+        {
+            if(randomRSP == "Rock")
+            {
+                PlayerWinCount++;
+                screen_s[(int)RCPNum.Rock].SetActive(false);
+                screen_s[7].SetActive(true);
+            }
+            else
+            {
+                screen_s[(int)RCPNum.Rock].SetActive(false);
+                screen_s[8].SetActive(false);
+                player.live = false;
+                gameManager.GameOver();
+            }
+        }
+        yield return new WaitForSeconds(2.0f);
+        isPlay = false;
     }
 }
